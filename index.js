06ac016894e7ffe8dd86b9b4355efd0a5034d578
Naurangi_lal <naurangilal15072002@gmail.com>
2024-03-08 13:29:49 +0530

    modofied files

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 😊: ');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file');
+      resolve('Successfully wrote file');
+    });
+  });
+};
+
+readFilePro('./txt/dog.txt')
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('./txt/dog-img2.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image to saved file');
+  })
+  .catch((err) => console.log(err.message));
 
 //Blocking synchronous way
 // const txtIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -10,7 +43,7 @@ const fs = require('fs');
 
 //Non-blocking Asynchronous way
 
-fs.readFile('./txt/readThat.txt', 'utf-8', (err, data) => {
-  console.log(data);
-});
-console.log('will file read');
+// fs.readFile('./txt/readThat.txt', 'utf-8', (err, data) => {
+//   console.log(data);
+// });
+// console.log('will file read');
